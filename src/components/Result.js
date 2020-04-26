db3cb5e37f4fc86e0cd83be660cf31036bbe6bd4
khushal <singhkhush552@gmail.com>
2020-04-26 22:36:31 +0530

    Initial Commit

---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Result = ({score, playAgain})=>(
+    <div className="score-board">
+
+        <div className="score">You scored {score}/5 correct answers</div>
+        <button className="playBtn" onClick={playAgain}> playAgain</button>
+    </div>
+);
+export default Result;
